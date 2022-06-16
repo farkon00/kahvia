0bfa14ae777d272ab9f8f0b19b6114ced2054e7a
Robin <robin@disking.co.uk>
2022-06-16 10:17:15 +0100

    Fix an error of TokenType not being defined

--- src/tokentype.py
+++ src/tokentype.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
+class TokenType(Enum):
+    IDENTIFIER = 0
+
 class Token():
     def __init__(self, typ: TokenType, val: str=''):
         self.typ: TokenType = typ
         self.val: str = val
-
-class TokenType(Enum):
-    IDENTIFIER = 0
