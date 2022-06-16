cfcbb0e9f52e82e659d50ca06befdf8567f94e9e
Robin <robin@disking.co.uk>
2022-06-16 09:20:41 +0100

    Initial commit!

---
+++ kahvia.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+
+def version():
+    print("[VERSION INFORMATION]")
+    print("    kahvia v0.0.1-alpha")
+
+def usage():
+    print("[USAGE]")
+    print("    ./kahvia.py <flags> [input file]")
+    print("[FLAGS]")
+    print("    -v, --version    Print the version information.")
+    print("    -h, --help       Print this message to stdout.")
+
+def error(err_msg):
+    print(f"kahvia: [ERROR] {err_msg}.")
+    usage()
+    exit(1)
+
+if __name__ == "__main__":
+    sys.argv = sys.argv[1:]
+    inp_file = ""f
+    flags = [] # This array will be appended to for each flag that doesn't halt execution (currently -h and -v)
+    for (i, argument) in enumerate(sys.argv):
+        if argument[0] == '-':
+            # Flags
+            if argument in ('-v', '--version'):
+                version()
+                exit(0)
+            elif argument in ('-h', '--help'):
+                usage()
+                exit(0)
+            else:
+                error("Unknown flag: " + argument)
+        else:
+            # File
+            if inp_file == "":
+                inp_file = argument
+            else:
+                error("Too many files provided. Only one input file may be supplied")
+
+    # We have the input file now :D
+    print(inp_file)
